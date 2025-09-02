266c79da52cd0ba0575f5089738c48daaf0f638e
Alexander Golod <steinegolod@gmail.com>
2025-09-02 22:03:47 +0200

    abaqus scripts upgraded to abaqus 2022 and choose consistent naming "pluginFuncions" for all import plugins

--- getMinMaxEnvelopeViewport_plugin.py
+++ getMinMaxEnvelopeViewport_plugin.py
@@ -1,48 +1,54 @@
-from abaqusGui import *
+import os
+
+import osutils
 from abaqusConstants import ALL
-import osutils, os
-
-
+from abaqusGui import *
 
 ###########################################################################
 # Class definition
 ###########################################################################
 
+
 class GetMinMaxEnvelopeViewport_plugin(AFXForm):
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__(self, owner):
-        
+
         # Construct the base class.
         #
         AFXForm.__init__(self, owner)
         self.radioButtonGroups = {}
 
-        self.cmd = AFXGuiCommand(mode=self, method='getMinMaxEnvelopeViewport',
-            objectName='myPluginFunctions', registerQuery=False)
-        pickedDefault = ''
+        self.cmd = AFXGuiCommand(
+            mode=self,
+            method="getMinMaxEnvelopeViewport",
+            objectName="pluginFuncions",
+            registerQuery=False,
+        )
+        pickedDefault = ""
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def getFirstDialog(self):
 
         import getMinMaxEnvelopeViewportDB
+
         return getMinMaxEnvelopeViewportDB.GetMinMaxEnvelopeViewportDB(self)
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def doCustomChecks(self):
 
         # Try to set the appropriate radio button on. If the user did
         # not specify any buttons to be on, do nothing.
         #
-        for kw1,kw2,d in self.radioButtonGroups.values():
+        for kw1, kw2, d in self.radioButtonGroups.values():
             try:
-                value = d[ kw1.getValue() ]
+                value = d[kw1.getValue()]
                 kw2.setValue(value)
             except:
                 pass
         return True
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def okToCancel(self):
 
         # No need to close the dialog when a file operation (such
@@ -50,7 +56,8 @@ class GetMinMaxEnvelopeViewport_plugin(AFXForm):
         #
         return False
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Register the plug-in
 #
 thisPath = os.path.abspath(__file__)
@@ -58,14 +65,14 @@ thisDir = os.path.dirname(thisPath)
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 toolset.registerGuiMenuButton(
-    buttonText='Get MinMax Envelope @ Viewport', 
+    buttonText="Get MinMax Envelope @ Viewport",
     object=GetMinMaxEnvelopeViewport_plugin(toolset),
     messageId=AFXMode.ID_ACTIVATE,
     icon=None,
-    kernelInitString='import myPluginFunctions',
-    applicableModules = ['Visualization'],
-    version='1.0',
-    author='Johannes Huber',
-    description='N/A',
-    helpUrl='N/A'
+    kernelInitString="import pluginFuncions",
+    applicableModules=["Visualization"],
+    version="1.0",
+    author="Johannes Huber",
+    description="N/A",
+    helpUrl="N/A",
 )
--- showNode_plugin.py
+++ showNode_plugin.py
@@ -1,50 +1,57 @@
-from abaqusGui import *
-from abaqusConstants import ALL
-import osutils, os
-from myIcons import showNodeIcon
-import i18n
+import os
 
+import i18n
+import osutils
+from abaqusConstants import ALL
+from abaqusGui import *
+from myIcons import showNodeIcon
 
 ###########################################################################
 # Class definition
 ###########################################################################
 
+
 class ShowNode_plugin(AFXForm):
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__(self, owner):
-        
+
         # Construct the base class.
         #
         AFXForm.__init__(self, owner)
         self.radioButtonGroups = {}
 
-        self.cmd = AFXGuiCommand(mode=self, method='showPickedNode',
-            objectName='myPluginFunctions', registerQuery=False)
-        pickedDefault = ''
-        self.myNodeKw = AFXObjectKeyword(self.cmd, 'myNode', TRUE, pickedDefault)
+        self.cmd = AFXGuiCommand(
+            mode=self,
+            method="showPickedNode",
+            objectName="pluginFuncions",
+            registerQuery=False,
+        )
+        pickedDefault = ""
+        self.myNodeKw = AFXObjectKeyword(self.cmd, "myNode", TRUE, pickedDefault)
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def getFirstDialog(self):
 
         import showNodeDB
+
         return showNodeDB.ShowNodeDB(self)
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def doCustomChecks(self):
 
         # Try to set the appropriate radio button on. If the user did
         # not specify any buttons to be on, do nothing.
         #
-        for kw1,kw2,d in self.radioButtonGroups.values():
+        for kw1, kw2, d in self.radioButtonGroups.values():
             try:
-                value = d[ kw1.getValue() ]
+                value = d[kw1.getValue()]
                 kw2.setValue(value)
             except:
                 pass
         return True
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def okToCancel(self):
 
         # No need to close the dialog when a file operation (such
@@ -52,7 +59,8 @@ class ShowNode_plugin(AFXForm):
         #
         return False
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Register the plug-in
 #
 thisPath = os.path.abspath(__file__)
@@ -60,39 +68,39 @@ thisDir = os.path.dirname(thisPath)
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 
-pluginDesc = i18n.tr( \
-    'A simple Node Extractor')
+pluginDesc = i18n.tr("A simple Node Extractor")
 
 # Register a GUI plug-in in the Plug-ins menu.
 #
 toolset.registerGuiMenuButton(
     object=ShowNode_plugin(toolset),
-    buttonText=i18n.tr('My Plugins|Get a Node Label'),
+    buttonText=i18n.tr("My Plugins|Get a Node Label"),
     messageId=AFXMode.ID_ACTIVATE,
     icon=None,
-    kernelInitString='import myPluginFunctions',
+    kernelInitString="import pluginFuncions",
     applicableModules=ALL,
-    #applicableModules = ['Visualization'],
-    version='1.0',
-    author='Johannes Huber',
+    # applicableModules = ['Visualization'],
+    version="1.0",
+    author="Johannes Huber",
     description=pluginDesc,
-    helpUrl='N/A'
+    helpUrl="N/A",
 )
 
 # Register a GUI plug-in in a toolbox.
 #
 icon = FXXPMIcon(getAFXApp(), showNodeIcon)
 
-toolset.registerGuiToolButton('My Toolbox', 
+toolset.registerGuiToolButton(
+    "My Toolbox",
     object=ShowNode_plugin(toolset),
-    buttonText=i18n.tr('\tFind a Node\nand show Label'),
-    kernelInitString='import myPluginFunctions',
+    buttonText=i18n.tr("\tFind a Node\nand show Label"),
+    kernelInitString="import pluginFuncions",
     icon=icon,
     applicableModules=ALL,
-    #applicableModules = ['Part', 'Property', 'Assembly', 'Step', 
+    # applicableModules = ['Part', 'Property', 'Assembly', 'Step',
     #    'Interaction', 'Load', 'Mesh', 'Job'],
-    version='1.0',
-    author='Johannes Huber',
+    version="1.0",
+    author="Johannes Huber",
     description=pluginDesc,
-    helpUrl='N/A'
+    helpUrl="N/A",
 )
